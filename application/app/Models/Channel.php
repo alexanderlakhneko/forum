326d81c3ba104b1d79b51c6ce9c9f427873d94e5
alexanderlakhneko <alexanderlakhneko@gmail.com>
2020-06-17 19:42:22 +0300

    Users Can Filter Threads By Channel

---
+++ application/app/Models/Channel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Channel
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Channel newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Channel newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Channel query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Channel whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Channel whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Channel whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Channel whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Channel whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Channel extends Model
+{
+    //
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function threads()
+    {
+        return $this->hasMany(Thread::class);
+    }
+}
